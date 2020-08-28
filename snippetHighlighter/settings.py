aedb1711aa9e4611941dd72f37cb1beb7d903753
luckypius50 <luckypius50@gmail.com>
2020-08-28 13:50:23 +0100

    :art: New module added!

--- snippetHighlighter/settings.py
+++ snippetHighlighter/settings.py
@@ -96,14 +96,11 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/staticfiles/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATIC_ROOT = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
-  os.path.join(BASE_DIR, 'static'),
+  os.path.join(BASE_DIR, 'staticfiles'),
 )
-
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
